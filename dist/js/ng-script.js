3e8dc3fdef3ab6d7b29b4d167040d43e71a9d969
ANIS ABID <anis.abid@atexo.com>
2017-05-10 16:25:43 +0200

    save

--- dist/js/ng-script.js
+++ dist/js/ng-script.js
@@ -74,16 +74,45 @@
 })(jQuery, KTJS);
 (function ($, $$) {
 
+
     $$.$({
         sidebar: {
             toggle: function () {
                 $('.js-kt-sidebar-pin').click(function () {
                     $('body').toggleClass('kt-sidebar--pin');
-                    console.log('click')
                 });
-                console.log('xx')
+            },
+            menu: {
+                fn: {
+                    open: function ($element) {
+                        $element.toggleClass('open active');
+                        $element.find('> ul').slideToggle();
+                    },
+                    close: function ($element) {
+                        this.open($element);
+                    }
+                },
+                toggle: function () {
+                    var _this = this,
+                        $sidebarMenu = $('.js-kt-sidebar-menu'),
+                        $subMenuList = $('.js-kt-sidebar-menu .sub-menu'),
+                        $sidebarMenuItemsList = $('.js-kt-sidebar-menu .menu-items li');
+
+                    $sidebarMenuItemsList.each(function (t) {
+                        var $this = $(this);
+                        if ($this.find('> ul').length) {
+                            $this.find('> a').click(function () {
+                                if (!$this.hasClass('open'))
+                                    _this.fn.close($sidebarMenu.find('.open'));
+                                _this.fn.open($this);
+                            });
+
+                        }
+                    })
+                }
             },
             ready: function () {
+                this.menu.toggle();
                 this.toggle();
             }
         }
